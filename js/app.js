8899af8aac34648fd201cc18c9aaf845efd46e01
thibault <thibault.daboville@gmail.com>
2017-02-13 19:26:21 +0100

    css terminé

--- js/app.js
+++ js/app.js
@@ -8,16 +8,18 @@ $(".button").click(function(){
     console.log(essai);
   });
 
-for(var i = 1; i<=3; i++){
 var essai = $(".input").children("input").val();
-console.log(essai);
-  if($(".button").click){
+for(var i = 1; i<=3; i++){
+  console.log(essai);
+  if($(".button").onclick){
+    console.log(essai);
      if (essai < nbMyst){
-      $("#ok").html('Vise un peu plus haut!');
+      $("#ok").html('Jen ai plus que ça!');
      } if (essai > nbMyst) {
       $("#ok").html('Un peu moins!');
      } if (essai == nbMyst) {
-      $("#ok").html("Bravo!");
+       console.log('bravo!');
+      $("#ok").html("Tu as gagné, tiens, prends ces anneaux, tu les as bien mérités");
        break;
      }
    }
